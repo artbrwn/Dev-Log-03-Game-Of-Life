d8cf74e8c33d6fae6b847dbdba43d36256bee7eb
Arturo <md.arturo@gmail.com>
2025-08-02 18:00:35 +0200

    Added new test for Cell

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -4,7 +4,7 @@ def test_cell_creation():
     test_cell = Cell([0, 0], [None, None, None, None, None, None, None, None])
     assert test_cell
 
-def test_update_state():
+def test_update_state_alive():
     target_cell = Cell([0, 0], 
                        [Cell([1, 1], [], alive=True), 
                        Cell([2, 2], [], alive=True), 
@@ -14,4 +14,12 @@ def test_update_state():
     target_cell.update_state()
     assert target_cell.alive
 
-    
+def test_update_state_dead():
+    target_cell = Cell([0, 0], 
+                       [Cell([1, 1], []), 
+                       Cell([2, 2], []), 
+                       Cell([3, 3], [])])
+    assert not target_cell.alive
+    target_cell.decide_next_state()
+    target_cell.update_state()
+    assert not target_cell.alive
