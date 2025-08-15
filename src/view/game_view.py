a5464f5a7916e6e36f0a07acc7cae36af75fb44c
Arturo <md.arturo@gmail.com>
2025-08-15 15:36:07 +0200

    Added visual representation for flux control bar

--- src/view/game_view.py
+++ src/view/game_view.py
@@ -1,21 +1,53 @@
 import pygame
 
 class GameView:
-    def __init__(self, universe, cell_size=10):
+    def __init__(self, universe, cell_size=10, navigation_bar_height=50):
             self.universe = universe
             self.cell_size = cell_size
             self.screen = pygame.display.set_mode(
-                (universe.cols * cell_size, universe.rows * cell_size)
-        )
+                (universe.cols * cell_size, universe.rows * cell_size + navigation_bar_height)
+                )
+            self.navigation_bar_height = navigation_bar_height
+            self.button_size = navigation_bar_height - navigation_bar_height * 0.2
     
     def draw(self):
-        self.screen.fill((0, 0, 0))
+        self.screen.fill(("#2B332B"))
+        # Draw cells (alive)
         for (row, col) in self.universe.cells:
-            rect = pygame.Rect(
+            cell_rect = pygame.Rect(
                 col * self.cell_size,
                 row * self.cell_size,
                 self.cell_size,
                 self.cell_size
             )
-            pygame.draw.rect(self.screen, (0, 255, 0), rect)
+            pygame.draw.rect(self.screen, (0, 255, 0), cell_rect)
+        # Draw flux control bar below
+        bar_rect = pygame.Rect(
+            0,
+            self.universe.rows * self.cell_size,
+            self.universe.cols * self.cell_size,
+            self.navigation_bar_height
+            )
+        pygame.draw.rect(self.screen, ("#395539"), bar_rect)
+        
+        # Draw pause, play, and next step buttons
+
+        # Coordinate in y of navigation bar bottom left corner
+        bar_bot_y = self.universe.rows * self.cell_size + self.navigation_bar_height
+
+        # Spacing between buttons
+        spacing = self.button_size // 2
+        
+        total_buttons_width = 3 * self.button_size + (3 - 1) * spacing
+
+        start_x = (self.universe.cols * self.cell_size - total_buttons_width) // 2
+
+        for i in range(3): 
+            button = pygame.Rect(
+                start_x + i * (self.button_size + spacing),
+                bar_bot_y - self.navigation_bar_height // 2 - self.button_size // 2,
+                self.button_size,
+                self.button_size
+            )
+            pygame.draw.rect(self.screen, (255, 255, 255), button)
         pygame.display.flip()
