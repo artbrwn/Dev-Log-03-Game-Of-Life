4da6393956c5733b435fb87c669a5591ee2a1d52
Arturo <md.arturo@gmail.com>
2025-08-24 15:08:59 +0200

    Added load_game_files_namesmethod to persistence class

--- src/persistence.py
+++ src/persistence.py
@@ -3,7 +3,8 @@ import os
 
 class Persistence:
     def __init__(self):
-        pass
+        self.saves_folder_name = "saves"
+        self.extension = ".csv"
 
     def save_state(self, universe):
         filename = self.get_next_file_name()
@@ -13,11 +14,11 @@ class Persistence:
             for coordinates in universe.cells.keys():
                 writer.writerow(coordinates)
 
-    def get_next_file_name(self, saves_folder_name= "saves", prefix="saved_game_", extension=".csv"):
+    def get_next_file_name(self, prefix="saved_game_"):
         # Create folder saves if it does not exist one level higher
         base_dir = os.path.dirname(__file__)
         project_dir = os.path.abspath(os.path.join(base_dir, ".."))
-        saves_dir = os.path.join(project_dir, saves_folder_name)
+        saves_dir = os.path.join(project_dir, self.saves_folder_name)
         if not os.path.exists(saves_dir):
             os.makedirs(saves_dir)
 
@@ -25,16 +26,27 @@ class Persistence:
         files = os.listdir(saves_dir)
         valid_files = [
             f for f in files 
-            if f.startswith(prefix) and f.endswith(extension)
+            if f.startswith(prefix) and f.endswith(self.extension)
             ]
         
         # List numbers in files
-        number_files = [int(file[len(prefix):-len(extension)]) for file in valid_files]
+        number_files = [int(file[len(prefix):-len(self.extension)]) for file in valid_files]
 
         # Get the biggest number in list
         max_number = max(number_files, default=0) + 1
         formatted_max_number = f"{max_number:03}"
-        file_name = prefix + formatted_max_number + extension
+        file_name = prefix + formatted_max_number + self.extension
 
         file_path = os.path.join(saves_dir, file_name)
         return file_path
+    
+    def load_game_files_names(self):
+        base_dir = os.path.dirname(__file__)
+        project_dir = os.path.abspath(os.path.join(base_dir, ".."))
+        saves_dir = os.path.join(project_dir, self.saves_folder_name)
+        all_files = os.listdir(saves_dir)
+        valid_files = [
+            f for f in all_files 
+            if f.endswith(self.extension)
+            ]
+        return valid_files
